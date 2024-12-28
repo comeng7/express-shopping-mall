5b3f201dd4053d1d6b5811d4c36eaf2bea3b1f27
정장오 매니저 <comeng7@hyundai.com>
2024-12-28 09:57:02 +0900

    CORS 설정

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import { specs } from '@/config/swagger';
 import productRouter from '@/routes/product.routes';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.NODE_ENV === 'production' ? `http://${process.env.EC2_PUBLIC_IP}` : 'http://localhost:3000',
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
