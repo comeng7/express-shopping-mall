a227c9f505567f3d66fc8e140047d99b93d9511d
정장오 매니저 <comeng7@hyundai.com>
2025-01-01 20:07:14 +0900

    cors 추가

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,7 @@ import productRouter from '@/routes/product.routes';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
