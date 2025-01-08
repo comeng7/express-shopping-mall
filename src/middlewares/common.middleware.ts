b4a13c730fd18ca1594c35bf75b35947d72485f2
정장오 매니저 <comeng7@hyundai.com>
2025-01-08 18:53:44 +0900

    cors 추가

--- src/middlewares/common.middleware.ts
+++ src/middlewares/common.middleware.ts
@@ -5,7 +5,12 @@ import helmet from 'helmet';
 export const commonMiddleware = [
   express.json(),
   express.urlencoded({ extended: true }),
-  cors(),
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }),
   helmet({
     contentSecurityPolicy: process.env.NODE_ENV === 'production' ? undefined : false,
   }),
